fa51cccdb080816b14775102c5a5403e48021136
RonexLemon <ronexondimu@gmail.com>
2025-07-22 18:15:24 +0300

    ft: Custom Chain connect

---
+++ src/providers/appProvider.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import '@rainbow-me/rainbowkit/styles.css';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  RainbowKitProvider,
+  connectorsForWallets,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider, createConfig, http } from 'wagmi';
+import { celo, celoAlfajores } from 'wagmi/chains';
+import { crossfi } from '@/chains/crossfi';
+import Layout from '../components/Layout';
+import { injectedWallet } from '@rainbow-me/rainbowkit/wallets';
+
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: 'Recommended',
+      wallets: [injectedWallet],
+    },
+  ],
+  {
+    appName: 'Nexo-Next',
+    projectId: process.env.WC_PROJECT_ID ?? '044601f65212332475a09bc14ceb3c34',
+  }
+);
+
+const config = createConfig({
+  connectors,
+  chains: [crossfi],
+  transports: {
+    [crossfi.id]: http()    
+  },
+});
+
+const queryClient = new QueryClient();
+
+export function AppProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Layout>{children}</Layout>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
